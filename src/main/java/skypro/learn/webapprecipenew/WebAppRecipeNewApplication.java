a0ff9cc338a121fefd6b8f654e9c5ed4c7927b58
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-12-09 15:41:36 +0300

    Домашняя работы от 9 декабря - передача файлов по HTTP

---
+++ src/main/java/skypro/learn/webapprecipenew/WebAppRecipeNewApplication.java
@@ -0,0 +1,13 @@
+package skypro.learn.webapprecipenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppRecipeNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppRecipeNewApplication.class, args);
+    }
+
+}
